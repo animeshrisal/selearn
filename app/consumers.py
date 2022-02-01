1812c1f76e4eec12f83dc8298e8343ddcf6cbf42
Animesh Risal <animeshrisal99@gmail.com>
2022-02-01 21:24:37 +0545

    Fix websocket

---
+++ app/consumers.py
@@ -0,0 +1,20 @@
+from channels.generic.websocket import AsyncJsonWebsocketConsumer
+import json
+
+class NotificationConsumer(AsyncJsonWebsocketConsumer):
+    async def connect(self):
+        self.user = self.scope['user'].username
+        self.room_group_name = "notification_" + self.user
+        await self.channel_layer.group_add(self.room_group_name, self.channel_name)
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room group
+        await self.channel_layer.group_discard(self.room_group_name, self.channel_name)
+
+    # Receive message from room group
+    async def notification_message(self, event):
+        message = event["message"]
+        
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
--- server/asgi.py
+++ server/asgi.py
@@ -6,11 +6,26 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
-
-import os
-
 from django.core.asgi import get_asgi_application
+django_asgi_app = get_asgi_application()
+import os
+from channels.routing import ProtocolTypeRouter, URLRouter
+from django.urls import re_path
+
+from app import consumers
+from .middleware import TokenAuthMiddleware
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
-application = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": TokenAuthMiddleware(
+        URLRouter([
+            re_path("", consumers.NotificationConsumer.as_asgi()),
+        ])
+    ),
+})
