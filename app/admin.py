92ae5c5ab1ac9b0d9f5f7985486499bf5a9bbd64
Animesh Risal <animeshrisal99@gmail.com>
2022-02-02 18:09:43 +0545

    Register models to admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Settings, Classroom, Lesson, Quiz, Question, UserAnswer, Comment
 
 # Register your models here.
+admin.site.register(Settings)
+admin.site.register(Classroom)
+admin.site.register(Lesson)
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(UserAnswer)
+admin.site.register(Comment)
+
