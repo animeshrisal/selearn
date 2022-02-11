b317eec797054c8aa879942e76de25a750ca55b6
Animesh Risal <animeshrisal99@gmail.com>
2022-02-11 22:13:24 +0545

    Fix asgi for docker and add celery

--- server/asgi.py
+++ server/asgi.py
@@ -7,15 +7,16 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 from django.core.asgi import get_asgi_application
-django_asgi_app = get_asgi_application()
+
+
 import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
+django_asgi_app = get_asgi_application()
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.urls import re_path
 
 from app import consumers
 from .middleware import TokenAuthMiddleware
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'server.settings')
 
 
 application = ProtocolTypeRouter({
