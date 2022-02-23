754f3a9e81c00dd7a999b2d3534c868505052658
Animesh Risal <animeshrisal99@gmail.com>
2022-02-23 16:24:57 +0545

    WIP navbar

--- client/src/modules/dashboard/components/NavBar.js
+++ client/src/modules/dashboard/components/NavBar.js
@@ -8,74 +8,86 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Avatar, Menu, MenuItem, Tooltip } from '@mui/material';
 import { useAuthentication, useSocket } from '../../shared/context';
+import { Navigate, useNavigate } from 'react-router-dom';
 
-const settings = ['Profile', 'Account', 'Dashboard'];
 
-const NavBar = () => {
-    const { state, dispatch } = useAuthentication();
-    const [anchorElNav, setAnchorElNav] = React.useState(null);
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const {disconnect} = useSocket();
-  
-    const handleOpenNavMenu = (event) => {
-      setAnchorElNav(event.currentTarget);
-    };
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-  
-    const handleCloseNavMenu = () => {
-      setAnchorElNav(null);
-    };
-  
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
 
-    const logoutUser = () => {
-        dispatch({ type: "LOGOUT" });
-        disconnect();
-      };
+const NavBar = (props) => {
+  let navigate = useNavigate();
+  const { state, dispatch } = useAuthentication();
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { disconnect } = useSocket();
 
-    return (
-        <AppBar position="fixed">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Dashboard
-                </Typography>
-                <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-            </Toolbar>
-        </AppBar>
+  const settings = [
+    {
+      title: 'Classes', method: () => 
+        goToClassroom()
+      
+    },
+    {
+      title: 'Logout', method: () => {
+        handleCloseUserMenu()
+      }
+    }
+  ];
 
-    )
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const goToClassroom = () => {
+    navigate(`classroom`)
+  }
+
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const logoutUser = () => {
+    dispatch({ type: "LOGOUT" });
+    disconnect();
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Dashboard
+        </Typography>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting.title} onClick={setting.method}>
+                <Typography textAlign="center">{setting.title}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+
+  )
 }
 
 export default NavBar
