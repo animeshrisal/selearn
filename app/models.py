cadbe4a618ecd9a37110ae1d79f82148bf892522
Animesh Risal <animeshrisal99@gmail.com>
2022-03-01 19:01:52 +0545

    Lint model file

--- app/models.py
+++ app/models.py
@@ -1,4 +1,3 @@
-from types import ClassMethodDescriptorType
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,8 +9,10 @@ from app.shared.models import TimeStampedModel
 class Settings(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
+
 class Classroom(TimeStampedModel):
-    students = models.ManyToManyField(User, related_name='enrolled_students', blank=True, through='Enrollment')
+    students = models.ManyToManyField(
+        User, related_name='enrolled_students', blank=True, through='Enrollment')
     teacher = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='teacher')
     subject = models.CharField(max_length=200)
@@ -23,11 +24,15 @@ class Classroom(TimeStampedModel):
     def __str__(self):
         return self.subject
 
+
 class Annoucement(TimeStampedModel):
     title = models.CharField(max_length=200)
     description = models.TextField()
-    teacher = models.ForeignKey(User, on_delete=models.CASCADE, related_name='teacher_annoucement')
-    classroom = models.ForeignKey(Classroom, on_delete=models.CASCADE, related_name='classroom_annoucement')
+    teacher = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='teacher_annoucement')
+    classroom = models.ForeignKey(
+        Classroom, on_delete=models.CASCADE, related_name='classroom_annoucement')
+
 
 class Enrollment(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -38,6 +43,7 @@ class Enrollment(models.Model):
     class Meta:
         unique_together = (('classroom_id', 'user_id'))
 
+
 class Lesson(TimeStampedModel):
     order = models.IntegerField()
     classroom = models.ForeignKey(
@@ -46,12 +52,13 @@ class Lesson(TimeStampedModel):
     name = models.CharField(max_length=200)
     description = models.TextField()
     body = models.TextField()
-    previous = models.ForeignKey('self', blank=True, null=True, related_name='previous_lesson', on_delete=models.SET_NULL)
-    next = models.ForeignKey('self', blank=True, null=True, related_name='next_lesson', on_delete=models.SET_NULL)
-    
+    previous = models.ForeignKey('self', blank=True, null=True,
+                                 related_name='previous_lesson', on_delete=models.SET_NULL)
+    next = models.ForeignKey('self', blank=True, null=True,
+                             related_name='next_lesson', on_delete=models.SET_NULL)
+
     class Meta:
         unique_together = (('id', 'order'))
-
 
     def __str__(self):
         return self.name
@@ -66,20 +73,22 @@ class UserLesson(models.Model):
     class Meta:
         unique_together = (('user', 'lesson'))
 
+
 class Quiz(TimeStampedModel):
     classroom = models.ForeignKey(
         Classroom, on_delete=models.CASCADE, related_name='quiz')
     student = models.ManyToManyField(User, blank=True, through='UserQuiz')
     name = models.CharField(max_length=200)
 
-
     def __str__(self):
         return self.name
+
 
 class UserQuiz(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     completed = models.BooleanField(default=False)
+
 
 class Question(TimeStampedModel):
     question = models.CharField(max_length=200)
@@ -88,13 +97,15 @@ class Question(TimeStampedModel):
     third_choice = models.TextField()
     fourth_choice = models.TextField()
     correct_choice = models.IntegerField()
-    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz_question')
+    quiz = models.ForeignKey(
+        Quiz, on_delete=models.CASCADE, related_name='quiz_question')
 
     user_answer = models.ManyToManyField(User, through='UserAnswer')
 
-
     def __str__(self):
         return self.question
+
+
 class UserAnswer(TimeStampedModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
