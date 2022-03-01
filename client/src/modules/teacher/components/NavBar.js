763f6dc7f312d57ebc2661b2408987e6d32f3f80
Animesh Risal <animeshrisal99@gmail.com>
2022-03-01 19:05:55 +0545

    Lint NavBar.js file

--- client/src/modules/teacher/components/NavBar.js
+++ client/src/modules/teacher/components/NavBar.js
@@ -10,64 +10,64 @@ import { useAuthentication, useSocket } from '../../shared/context';
 const settings = ['Logout'];
 
 const NavBar = () => {
-    const { state, dispatch } = useAuthentication();
+  const { state, dispatch } = useAuthentication();
 
-    const [anchorElUser, setAnchorElUser] = React.useState(null);
-    const {disconnect} = useSocket();
-  
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const { disconnect } = useSocket();
 
-    const handleOpenUserMenu = (event) => {
-      setAnchorElUser(event.currentTarget);
-    };
-   
-    const handleCloseUserMenu = () => {
-      setAnchorElUser(null);
-    };
 
-    const logoutUser = () => {
-        dispatch({ type: "LOGOUT" });
-        disconnect();
-      };
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-    return (
-        <AppBar position="fixed">
-            <Toolbar>
-                <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-                    Dashboardasdasdasd
-                </Typography>
-                <Box sx={{ flexGrow: 0 }}>
-            <Tooltip title="Open settings">
-              <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-                <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
-              </IconButton>
-            </Tooltip>
-            <Menu
-              sx={{ mt: '45px' }}
-              id="menu-appbar"
-              anchorEl={anchorElUser}
-              anchorOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              keepMounted
-              transformOrigin={{
-                vertical: 'top',
-                horizontal: 'right',
-              }}
-              open={Boolean(anchorElUser)}
-              onClose={handleCloseUserMenu}
-            >
-              {settings.map((setting) => (
-                <MenuItem key={setting} onClick={handleCloseUserMenu}>
-                  <Typography textAlign="center">{setting}</Typography>
-                </MenuItem>
-              ))}
-            </Menu>
-          </Box>
-            </Toolbar>
-        </AppBar>
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
-    )
+  const logoutUser = () => {
+    dispatch({ type: "LOGOUT" });
+    disconnect();
+  };
+
+  return (
+    <AppBar position="fixed">
+      <Toolbar>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Dashboardasdasdasd
+        </Typography>
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+              <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
+        </Box>
+      </Toolbar>
+    </AppBar>
+
+  )
 }
 
 export default NavBar
