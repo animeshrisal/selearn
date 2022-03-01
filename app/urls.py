692f2feae54fb9e044df68b97b02910d451ee4d3
Animesh Risal <animeshrisal99@gmail.com>
2022-03-01 19:02:07 +0545

    Lint urls

--- app/urls.py
+++ app/urls.py
@@ -18,19 +18,24 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('classroom/', views.ClassroomViewSet.as_view({'get': 'list', 'post': 'create'}), name="classroom"),
+    path('classroom/', views.ClassroomViewSet.as_view(
+        {'get': 'list', 'post': 'create'}), name="classroom"),
     path('classroom/<int:pk>/',
          views.ClassroomViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="classroom-detail"),
-    path('classroom/<int:pk>/lesson', views.LessonListCreateAPI.as_view(), name="class_lesson"),
-    path('classroom/<int:pk>/user_lesson', views.UserLessonListAPI.as_view(), name="user_lesson"),
-    path('classroom/<int:pk>/lesson/<int:lesson_pk>', views.UserLessonRetrieveUpdateDestroyAPI.as_view(), name="lesson-detail"),
-    
-    path('classroom/<int:pk>/enroll', views.EnrollStudentAPI.as_view(), name="enroll"),
-    path('classroom/<int:pk>/lesson/<int:lesson_pk>/complete', views.CompleteLessonAPI.as_view(), name="complete_lesson"),
-    path('classroom/<int:pk>/quiz/', views.ClassroomQuizAPI.as_view({'get': 'list', 'post': 'create'}), name="quiz"),
+    path('classroom/<int:pk>/lesson',
+         views.LessonListCreateAPI.as_view(), name="class_lesson"),
+    path('classroom/<int:pk>/user_lesson',
+         views.UserLessonListAPI.as_view(), name="user_lesson"),
+    path('classroom/<int:pk>/lesson/<int:lesson_pk>',
+         views.UserLessonRetrieveUpdateDestroyAPI.as_view(), name="lesson-detail"),
+
+    path('classroom/<int:pk>/enroll',
+         views.EnrollStudentAPI.as_view(), name="enroll"),
+    path('classroom/<int:pk>/lesson/<int:lesson_pk>/complete',
+         views.CompleteLessonAPI.as_view(), name="complete_lesson"),
+    path('classroom/<int:pk>/quiz/',
+         views.ClassroomQuizAPI.as_view({'get': 'list', 'post': 'create'}), name="quiz"),
     path('classroom/<int:pk>/quiz/<int:quiz_pk>',
          views.ClassroomQuizAPI.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name="quiz"),
-   
+
 ]
-
-
